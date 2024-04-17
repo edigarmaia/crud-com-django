c4e5056c50a98fc5ec040a4df7effca7dc657bd8
Edigar Maia <edigarmaia@gmail.com>
2024-04-16 23:24:17 -0300

    CREATE

--- core/views.py
+++ core/views.py
@@ -5,3 +5,11 @@ from .models import Pessoa
 def home(request):
     pessoas = Pessoa.objects.all()
     return render(request, 'index.html', {'pessoas': pessoas})
+
+
+def salvar(request):
+    vnome = request.POST.get("nome")
+    Pessoa.objects.create(nome=vnome)
+    pessoas = Pessoa.objects.all()
+    return render(request, 'index.html', {'pessoas': pessoas})
+
